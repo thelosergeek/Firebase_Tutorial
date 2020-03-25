ad93fd08506735b423941c0aa9ed27502fe65874
Inam Khan <inamkh0001@gmail.com>
2020-03-25 11:11:11 +0530

    Added Phone Authentication

--- app/src/main/java/com/example/logindemo/Registration.java
+++ app/src/main/java/com/example/logindemo/Registration.java
@@ -9,7 +9,8 @@ import android.widget.Button;
 
 public class Registration extends AppCompatActivity {
 
-    private Button regist;
+    private Button regist, git, phone, google;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,19 @@ public class Registration extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         regist = (Button) findViewById(R.id.btnemail);
+        git = (Button) findViewById(R.id.btgithub);
+        phone = (Button) findViewById(R.id.btphone);
+        google = (Button) findViewById(R.id.btgoogle);
+
+
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Registration.this, phonereg.class));
+            }
+        });
+
+
         regist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
